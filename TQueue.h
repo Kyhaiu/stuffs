32e3051ee6d9edac9f6bf1783a79931b2b8c6354
Marcos Augusto Campagnaro <38305986+Kyhaiu@users.noreply.github.com>
2019-11-25 19:16:13 -0300

    Add files via upload

---
+++ TQueue.h
@@ -0,0 +1,28 @@
+#ifndef TQUEUE_H_INCLUDED
+#define TQUEUE_H_INCLUDED
+#include "TLista.h"
+
+template<typename T> class TQueue{
+    private:
+        TLista<T> *Q;
+    public:
+        TQueue<T>();
+        void enqueue(T _info);
+        T dequeue();
+};
+
+template<typename T>TQueue<T>::TQueue(){
+    Q = new TLista<T>();
+}
+
+template<typename T> void TQueue<T>::enqueue(T _info){
+    Q->ins_fim(_info);
+}
+
+template<typename T> T TQueue<T>::dequeue(){
+    T aux = Q->getprim()->getinfo();
+    Q->rem_ini();
+    return aux;
+}
+
+#endif // TQUEUE_H_INCLUDED
